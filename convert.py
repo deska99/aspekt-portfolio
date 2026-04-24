40f42868f25fa4be029a05edfab6aa4a597ed3dc
Krzysztof Iłendo <krzysztofilendo@Krzysztofs-MacBook-Pro.local>
2026-04-24 15:29:06 +0200

    fix: Remove OFERTA link and optimize hero images on Opinions & Stories

---
+++ convert.py
@@ -0,0 +1,3 @@
+from PIL import Image
+Image.open("opinions/ASPEKT-pIK-199.jpg").save("opinions/ASPEKT-pIK-199.webp", format="webp")
+Image.open("stories/ASPEKT-pSM-62.jpg").save("stories/ASPEKT-pSM-62.webp", format="webp")
